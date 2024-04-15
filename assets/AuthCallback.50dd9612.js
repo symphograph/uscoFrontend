86b45b7dfeb3a225205e37bac745d44e2fab3193
symphograph <58216294+symphograph@users.noreply.github.com>
2024-04-15 13:28:56 +0000

    Deploying to gh-pages from @ symphograph/uscoFrontend@1b3195ad39d777657cebbe9a2a2bfe4cf57588ed 🚀

---
+++ assets/AuthCallback.50dd9612.js
@@ -0,0 +1,1 @@
+import{P as r}from"./LocalStorage.049c88e1.js";import{I as a,o as s}from"./index.cdb55744.js";const m={__name:"AuthCallback",setup(u){const o=a();function e(){var t;o.push({path:(t=r.getItem("lastPath"))!=null?t:"/"})}return s(()=>{e()}),()=>{}}};export{m as default};
