e284b3a60109d06071526be0b215cd2f8ed45b18
symphograph <58216294+symphograph@users.noreply.github.com>
2024-05-24 11:14:27 +0000

    Deploying to gh-pages from @ symphograph/uscoFrontend@96d462142c55334d9e9ff5a35a769584ab947f71 🚀

---
+++ assets/QToolbarTitle.8226ac31.js
@@ -0,0 +1,1 @@
+import{S as t,c as l,h as a,U as r}from"./index.204d8958.js";t({name:"QToolbarTitle",props:{shrink:Boolean},setup(s,{slots:e}){const o=l(()=>"q-toolbar__title ellipsis"+(s.shrink===!0?" col-shrink":""));return()=>a("div",{class:o.value},r(e.default))}});
--- assets/QToolbarTitle.bed504ab.js
+++
@@ -1,1 +0,0 @@
-import{S as t,c as l,h as a,U as r}from"./index.75c46ebb.js";t({name:"QToolbarTitle",props:{shrink:Boolean},setup(s,{slots:e}){const o=l(()=>"q-toolbar__title ellipsis"+(s.shrink===!0?" col-shrink":""));return()=>a("div",{class:o.value},r(e.default))}});
