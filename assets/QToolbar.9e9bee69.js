bc53924b8edb039a0c19667109396f0aabab6759
symphograph <58216294+symphograph@users.noreply.github.com>
2024-08-12 16:11:57 +0000

    Deploying to gh-pages from @ symphograph/uscoFrontend@ef80cc199b0e6ec70a044a8bb9216a0bc92319cb 🚀

---
+++ assets/QToolbar.9e9bee69.js
@@ -0,0 +1,1 @@
+import{S as a,c as r,h as s,U as l}from"./index.a3581ca9.js";var p=a({name:"QToolbar",props:{inset:Boolean},setup(o,{slots:e}){const t=r(()=>"q-toolbar row no-wrap items-center"+(o.inset===!0?" q-toolbar--inset":""));return()=>s("div",{class:t.value,role:"toolbar"},l(e.default))}});export{p as Q};
